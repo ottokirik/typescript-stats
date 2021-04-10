8e75bf5dfb95dd8d7ccc133fdf6cec38f6c14d7a
Petrov Kirill <ottokirik@gmail.com>
2021-04-11 01:53:05 +0300

    Первый вариант рефакторинга класса CsvReader

--- build/index.js
+++ build/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var CsvFileReader_1 = require("./CsvFileReader");
+var MatchReader_1 = require("./MatchReader");
 var MatchResult_1 = require("./MatchResult");
-var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+var reader = new MatchReader_1.MatchReader('football.csv');
 reader.read();
 var manUnitedWins = reader.data.reduce(function (acc, itemRow) {
     return (itemRow[1] === 'Man United' && itemRow[5] === MatchResult_1.MatchResult.HomeWin) ||
